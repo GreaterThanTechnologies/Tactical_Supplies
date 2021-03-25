30c3b08a4fc7937ff0624579b1c344a2ff1f0ce7
prodigysurge <boatinglife6@yahoo.com>
2021-03-25 15:07:05 -0400

    adds tables

--- db/migrate/20210325185547_create_owners.rb
+++ db/migrate/20210325185547_create_owners.rb
@@ -1,6 +1,10 @@
 class CreateOwners < ActiveRecord::Migration[6.1]
   def change
     create_table :owners do |t|
+      t.string :name
+      t.string :username
+      t.string :company_name
+      t.string :password_digest
 
       t.timestamps
     end
--- db/migrate/20210325185705_create_supplies.rb
+++ db/migrate/20210325185705_create_supplies.rb
@@ -1,6 +1,12 @@
 class CreateSupplies < ActiveRecord::Migration[6.1]
   def change
     create_table :supplies do |t|
+      t.string :unit 
+      t.integer :quantity
+      t.integer :item_id
+      t.integer :owner_id
+        
+       
 
       t.timestamps
     end
